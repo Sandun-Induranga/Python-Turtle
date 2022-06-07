50f47148829d396344fb52c3d38c92f8fb9e38e7
Sandun-Induranga <sandutharu40@gmail.com>
2022-06-08 00:58:55 +0530

    Set color for square

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import turtle
 
 t = turtle.Turtle()
 
+t.color('blue', '#a2d2ff')
+
+t.begin_fill()
 t.forward(100)
 t.left(90)
 t.forward(100)
@@ -9,5 +12,6 @@ t.left(90)
 t.forward(100)
 t.left(90)
 t.forward(100)
+t.end_fill()
 
 turtle.done()
