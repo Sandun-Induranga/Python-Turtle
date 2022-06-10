29e2a1a0cbe0fc6371900c905b3d44c16b75e070
Sandun-Induranga <sandutharu40@gmail.com>
2022-06-10 23:11:03 +0530

    Content Updated

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 import turtle
 
+# Lines
+
+# t = turtle.Turtle()
+#
+# t.forward(100)
+# t.left(45)
+# t.forward(100)
+# t.right(90)
+# t.forward(100)
+#
+# turtle.done()
+
+# Squares
+
 t = turtle.Turtle()
 
 t.color('blue', '#a2d2ff')
@@ -13,5 +27,6 @@ t.forward(100)
 t.left(90)
 t.forward(100)
 t.end_fill()
+
 
 turtle.done()
