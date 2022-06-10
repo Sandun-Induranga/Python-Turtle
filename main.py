2432f7984c0105d5e0f1e787fdb12d1f771925f0
Sandun-Induranga <sandutharu40@gmail.com>
2022-06-10 23:16:08 +0530

    Two Squares

--- main.py
+++ main.py
@@ -28,5 +28,18 @@ t.left(90)
 t.forward(100)
 t.end_fill()
 
+t.penup()
+t.forward(150)
+t.pendown()
+
+t.begin_fill()
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.end_fill()
 
 turtle.done()
